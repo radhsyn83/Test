e96f11a8aa0be59b01f47fbad2e15e09f45e92f2
radhsyn83 <fathur.radhy@yahoo.com>
2015-02-09 20:23:59 +0700

    Update ujicom.sh

--- ujicom.sh
+++ ujicom.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-/etc/init.d/networking stop
+/etc/init.d/squid3 stop
 cp /etc/squid3/squid3.conf /etc/squid3/squid3.conf.ASLI
 rm /etc/squid3/squid3.conf
 wget https://github.com/radhsyn83/Test/blob/master/squid3.conf
 mv squid3.conf /etc/squid3/
-/etc/init.d/networking restart
+/etc/init.d/squid3 restart
